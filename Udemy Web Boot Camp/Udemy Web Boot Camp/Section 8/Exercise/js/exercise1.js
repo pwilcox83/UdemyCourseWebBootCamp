f578499a8dc53ee85e0698ef93617edea9f40dcb
Paul Wilcox <pwilcox83@gmail.com>
2018-08-18 19:27:08 +0100

    section 8 done

---
+++ Udemy Web Boot Camp/Udemy Web Boot Camp/Section 8/Exercise/js/exercise1.js
@@ -0,0 +1,5 @@
+﻿var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
---
+++ Udemy Web Boot Camp/Udemy Web Boot Camp/Section 8/Exercise/js/exercise2.js
@@ -0,0 +1,2 @@
+﻿var daysalive = prompt("Whats your age");
+console.log("You are " + daysalive * 365 * 1.25 + " days old" )
