276b48017a47faf5b409016d85339a25b50a313c
Paul Wilcox <paul.wilcox@sqa.org.uk>
2018-08-29 16:42:09 +0100

    section 12 done

---
+++ Udemy Web Boot Camp/Udemy Web Boot Camp/Section 12/Exercise/js/exercise1.js
@@ -0,0 +1,18 @@
+﻿var movies = [
+    { title: "In Bruges", stars: 5, viewed: true },
+    { title: "Frozen", stars: 4.5, viewed: false },
+    { title: "Mad Max", stars: 5, viewed: true },
+    { title: "Les Miserables", stars: 3.5, viewed: false }];
+
+function listMovies(arr) {
+    arr.forEach(function (movie) {
+        var prepend = "";
+        if (movie.viewed)
+            prepend = "You have watched ";
+        else
+            prepend = "You have not seen ";
+        console.log(prepend + "\"" + movie.title + "\" - " + movie.stars + " stars");
+    });
+}
+listMovies(movies);
+
