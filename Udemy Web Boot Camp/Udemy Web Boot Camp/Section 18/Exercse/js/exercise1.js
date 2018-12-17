dbd48fe6ffebb0be65375851c1e34d0e5e696f65
Paul Wilcox <paul.wilcox@sqa.org.uk>
2018-12-17 07:04:43 +0000

    finished sections 16 - 18

---
+++ Udemy Web Boot Camp/Udemy Web Boot Camp/Section 18/Exercse/js/exercise1.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $("div").css("background", "purple");
+    $(".highlight").css("width", "200px");
+    $("#third").css("border", "orange 3px solid");
+    var divs = $("div");
+    $(divs[0]).css("border", "pink 3px solid");
+});
