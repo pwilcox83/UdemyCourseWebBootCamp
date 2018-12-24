6e4071a0998c5b1c07a11b7b63ee4c0ea949a1a5
Paul Wilcox <paul.wilcox@sqa.org.uk>
2018-12-24 14:21:33 +0000

    section complete

---
+++ Udemy Web Boot Camp/Udemy Web Boot Camp/Section 21/Code Along/assets/js/todo.js
@@ -0,0 +1,29 @@
+﻿var keyData = {
+    a:{
+    color:"purple",
+    sound = new Howl({src: ["assets/sounds/bubbles.mp3"]})
+    },
+    s:{
+    color:"green",
+    sound = new Howl({src: ["assets/sounds/clay.mp3"]})
+    },
+    s:{
+    color:"yellow",
+    sound = new Howl({src: ["assets/sounds/confetti.mp3"]})
+    }};
+    var circles = [];
+
+    function onKeyDown(event){
+        var maxPoint = new Point(view.size.width, view.size.height);
+        var randomPoint = Point.random();
+        var point = maxPoint * randomPoint;
+        var newCircle = new Path.Circle(point, 500);
+        newCircle.fillColor =  keyData[event.key].color;
+        //keyData[event.key].sound.play();
+
+        else{
+            newCircle.fillColor = "orange";
+        }
+
+        circles.push(newCircle);
+    }
